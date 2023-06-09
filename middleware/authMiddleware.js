dfe6100fb3d8567fa5e9d4e34e90991c02675bf4
Majedul Hasan <hasanmajedul@gmail.com>
2023-06-09 15:34:57 +0600

    tokenPost controller func

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,4 +1,5 @@
 const jwt = require('jsonwebtoken');
+const generateToken = require('../utils/generateToken');
 
 const verifyJWT = (req, res, next) => {
   const authorization = req.headers.authorization;
@@ -10,7 +11,7 @@ const verifyJWT = (req, res, next) => {
   }
   const token = authorization.split(' ')[1];
   console.log(token);
-  jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
     if (err) {
       return res.status(401).send({ error: true, message: err });
     }
@@ -19,4 +20,17 @@ const verifyJWT = (req, res, next) => {
   });
 };
 
-module.exports = { verifyJWT };
+const tokenPost = (req, res) => {
+  const user = req.body;
+  const token = generateToken(user);
+  // Set the cookie with the JWT token
+  // send token in cookie
+  res.cookie('token', token, {
+    httpOnly: true,
+    secure: true, // only works on https
+  });
+
+  res.send({ token });
+};
+
+module.exports = { verifyJWT, tokenPost };
