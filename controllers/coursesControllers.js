51f2b30c4c28d7f011112a9b95f016d9cb07d293
Majedul Hasan <hasanmajedul@gmail.com>
2023-06-30 16:40:05 +0600

    course api redux

--- controllers/coursesControllers.js
+++ controllers/coursesControllers.js
@@ -4,10 +4,12 @@ const Course = require('../models/courseModel');
 
 const getCoursesCtrl = asyncHandler(async (req, res) => {
   const { limit } = req.query;
-  const limInt = parseInt(limit) || 0;
+  const limInt = parseInt(limit);
 
   const all = await Course.find({ status: 'active' })
     .populate('instructor', '_id name email image')
+    .limit(limInt)
+
     .exec();
   res.json(all);
 });
